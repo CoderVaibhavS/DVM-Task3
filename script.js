11fe6a97b08674fb486b2a0b94d066c209028f61
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2022-03-16 14:39:20 +0530

    major animation changes

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-document.querySelector('.visit-1').addEventListener('mouseover', function run(){
+document.querySelector('.visit-1').addEventListener('mouseover', function run1(){
     document.querySelector('.left-1').style.transform = 'translateX(-5px)';
-    document.querySelector('.visit-1').addEventListener('mouseout', function run(){
-        document.querySelector('.left-1').style.transform = 'translateX(0)';
-    })
+
+    document.getElementById('visit-vid-1').outerHTML = 
+    `<video class="video" id="visit-vid-1" src="visit-vid-1.mp4" poster="visit-img-1.jpg" autoplay loop muted></video>`;
 })
 
-document.querySelector('.visit-2').addEventListener('mouseover', function run(){
-    document.querySelector('.right-1').style.transform = 'translateX(5px)';
-    document.querySelector('.visit-2').addEventListener('mouseout', function run(){
-        document.querySelector('.right-1').style.transform = 'translateX(0)';
-    })
+document.querySelector('.visit-1').addEventListener('mouseout', function run2(){
+
+    document.querySelector('.left-1').style.transform = 'translateX(0)';
+    document.getElementById('visit-vid-1').outerHTML = 
+    `<video class="video" id="visit-vid-1" src="visit-vid-1.mp4" poster="visit-img-1.jpg"></video>`;
 })
+
+document.querySelector('.visit-2').addEventListener('mouseover', function run3(){
+    document.querySelector('.right-1').style.transform = 'translateX(5px)';
+
+    document.getElementById('visit-vid-2').outerHTML = 
+    `<video class="video" id="visit-vid-2" src="visit-vid-2.mp4" poster="visit-img-2.jpg" autoplay loop muted></video>`;
+})
+
+document.querySelector('.visit-2').addEventListener('mouseout', function run4(){
+    document.querySelector('.right-1').style.transform = 'translateX(0)';
+
+    document.getElementById('visit-vid-2').outerHTML = 
+    `<video class="video" id="visit-vid-2" src="visit-vid-2.mp4" poster="visit-img-2.jpg"></video>`;
+})
+
+document.getElementById('three-mile-greenway').addEventListener('mouseover', function run(){
+    document.querySelector('#three-mile-greenway-vid').style.display = 'block';
+})
+
+document.getElementById('three-mile-greenway').addEventListener('mouseout', function run(){
+    document.querySelector('#three-mile-greenway-vid').style.display = 'none';
+})
+
+
 
 /**************************************************************************************/
 // document.querySelector('#wheelchair').addEventListener('mouseover', function run(){
